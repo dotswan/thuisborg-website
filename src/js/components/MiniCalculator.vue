45026e24e9df342f8fdb28c3850c7e55a2bffd3f
oleksandr <slimnation2@gmail.com>
2024-10-18 16:24:36 +0200

    index Done

--- src/js/components/MiniCalculator.vue
+++ src/js/components/MiniCalculator.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="mini-calculator">
     <div class="title-lang">
-      <h3>{{ Translate("Periodic Income Planner", currentLang) }}</h3>
+      <h3>{{ translate('title') }}</h3>
       <div class="lang-switcher">
         <select id="lang" v-model="currentLang">
           <option value="nl">NL</option>
@@ -12,14 +12,14 @@
     </div>
     <div class="fields-wrapper">
       <div class="field-row">
-        <label>{{ Translate("What is your current home value?", currentLang) }}</label>
+        <label>{{ translate('currentHomeValue') }}</label>
         <div class="value-cont">
           <vue-slider :min="50000" :max="1000000" :interval="1" :dotSize="30" :height="12" direction="ltr" v-model="fields.f1"></vue-slider>
           <input type="text" v-model="fields.f1" />
         </div>
       </div>
       <div class="field-row">
-        <label>{{ Translate("What is your current home value?", currentLang) }}</label>
+        <label>{{ translate('currentHomeValue') }}</label>
         <div class="value-cont">
           <vue-slider :min="500" :max="10000" :interval="1" :dotSize="30" :height="12" direction="ltr" v-model="fields.f2"></vue-slider>
           <input type="text" v-model="fields.f2" />
@@ -28,35 +28,83 @@
     </div>
     <div class="results-wrapper">
       <p class="result-row">
-        {{ Translate("You may receive from Thuisborg for your house", currentLang) }}
-        {{ Translate("To", currentLang) }}
-        <span class="color-navy">125 {{ Translate("% of house value", currentLang) }}</span>
-        {{ Translate("This results in a staggered receipt of payment of the purchase price:", currentLang) }}
-        {{ Translate("From", currentLang) }}
+        {{ translate('youMayReceive') }} 
+        <span class="color-navy">125 {{ translate('percentOfHouseValue') }}</span>
+        {{ translate('thisResultsIn') }} 
         <span class="color-navy">{{ startDate }}</span>
-        {{ Translate("To", currentLang) }}
+        {{ translate('to') }} 
         <span class="color-navy">{{ endDate }}</span>
-        {{ Translate("An amount of", currentLang) }}
+        {{ translate('anAmountOf') }} 
         <span class="color-navy">
-          {{ fields.f2 }}
-          {{ Translate("EUR per month", currentLang) }}
+          {{ fields.f2 }} {{ translate('eurPerMonth') }}
         </span>
-        {{ Translate("After", currentLang) }}
-        <span class="color-navy">
-          {{ endDate }}
-        </span>
-        {{ Translate("You may continue to live in your home as long as you want", currentLang) }}
+        {{ translate('after') }} 
+        <span class="color-navy">{{ endDate }}</span>
+        {{ translate('youMayContinueToLive') }}
         <span>.</span>
       </p>
       <div class="result-row flex-end">
-        <a @click="saveLocal()" class="link-to" href="/woningwaarde-calculator">{{ Translate("detailed calculator", currentLang) }} <i>></i></a>
+        <a @click="saveLocal()" class="link-to" href="/woningwaarde-calculator">
+          {{ translate('detailedCalculator') }} <i>></i>
+        </a>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+const translations = {
+  nl: {
+    title: "Periodieke Inkomensplanner",
+    currentHomeValue: "Wat is uw huidige woningwaarde?",
+    youMayReceive: "U kunt ontvangen van Thuisborg voor uw huis",
+    percentOfHouseValue: "% van de woningwaarde",
+    thisResultsIn: "Dit resulteert in een gespreide ontvangst van de koopprijs:",
+    to: "Tot",
+    anAmountOf: "Een bedrag van",
+    eurPerMonth: "EUR per maand",
+    after: "Na",
+    youMayContinueToLive: "U kunt blijven wonen in uw huis zolang u wilt",
+    detailedCalculator: "gedetailleerde calculator",
+  },
+  en: {
+    title: "Periodic Income Planner",
+    currentHomeValue: "What is your current home value?",
+    youMayReceive: "You may receive from Thuisborg for your house",
+    percentOfHouseValue: "% of house value",
+    thisResultsIn: "This results in a staggered receipt of payment of the purchase price:",
+    to: "To",
+    anAmountOf: "An amount of",
+    eurPerMonth: "EUR per month",
+    after: "After",
+    youMayContinueToLive: "You may continue to live in your home as long as you want",
+    detailedCalculator: "detailed calculator",
+  },
+  it: {
+    title: "Pianificatore di Reddito Periodico",
+    currentHomeValue: "Qual è il valore attuale della tua casa?",
+    youMayReceive: "Puoi ricevere da Thuisborg per la tua casa",
+    percentOfHouseValue: "% del valore della casa",
+    thisResultsIn: "Questo si traduce in un pagamento dilazionato del prezzo di acquisto:",
+    to: "A",
+    anAmountOf: "Un importo di",
+    eurPerMonth: "EUR al mese",
+    after: "Dopo",
+    youMayContinueToLive: "Puoi continuare a vivere nella tua casa finché vuoi",
+    detailedCalculator: "calcolatore dettagliato",
+  },
+};
+
 export default {
+  data() {
+    return {
+      currentLang: this.$store.state.currentLang,
+      fields: {
+        f1: 350000,
+        f2: 2000,
+      },
+    };
+  },
   computed: {
     startDate() {
       const date = new Date().toLocaleString("en-US", {
@@ -67,17 +115,8 @@ export default {
       return date;
     },
     endDate() {
-      /*
-       * k10 = f1  (price)
-       * k48 = 125 (percentage)
-       * G17 = 125 (percentage)
-       * K12 = f2
-       * */
-      //formula =TODAY() + ( K10 * Parameters!K48 / 100 ) * G17 / 100 / ( K12 * 12 ) * 365.24219
-      let today = new Date(),
-        days,
-        total;
-      days = ((((this.fields.f1 * this.constants.k48) / 100) * this.constants.G17) / 100 / (this.fields.f2 * 12)) * 365.24219;
+      let today = new Date(), days, total;
+      days = ((((this.fields.f1 * 125) / 100) * 125) / 100 / (this.fields.f2 * 12)) * 365.24219;
       total = new Date(today.setDate(today.getDate() + days));
       return total.toLocaleString("en-US", {
         year: "numeric",
@@ -86,21 +125,10 @@ export default {
       });
     },
   },
-  props: {},
-  data() {
-    return {
-      constants: {
-        k48: 125,
-        G17: 125,
-      },
-      currentLang: "nl",
-      fields: {
-        f1: 350000,
-        f2: 2000,
-      },
-    };
-  },
   methods: {
+    translate(key) {
+      return translations[this.currentLang][key] || key;
+    },
     setUp() {
       if (localStorage.getItem("calculator")) {
         this.fields = JSON.parse(localStorage.getItem("calculator")).fields;
@@ -117,9 +145,13 @@ export default {
     this.setUp();
   },
   watch: {
-    currentLang() {
-      this.langSwitch();
+    currentLang(newLang) {
+      this.$store.commit('setCurrentLang', newLang);
     },
   },
 };
 </script>
+
+<style scoped>
+/* Додайте стилі для компонента тут */
+</style>
--- src/js/store.js
+++ src/js/store.js
@@ -3,16 +3,29 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     baseUrl: '',
+    currentLang: 'en', // Встановіть значення за замовчуванням
   },
   mutations: {
     setBaseUrl(state) {
       state.baseUrl = window.location.origin;
     },
+    detectLanguage(state) {
+      const qs = new URL(document.location);
+      state.currentLang = qs.pathname.includes('/nl') ? 'nl' : 'en'; // Встановіть мову на основі URL
+    },
+    setCurrentLang(state, lang) {
+      state.currentLang = lang; // Додайте мутацію для зміни мови
+    },
   },
   actions: {
-    setUp({ commit, state }) {
-      console.log('setUp action called');
+    setUp({ commit }) {
       commit("setBaseUrl");
+      commit('detectLanguage');
+    },
+  },
+  getters: {
+    currentLang: state => {
+      return state.currentLang;
     },
   },
 });
